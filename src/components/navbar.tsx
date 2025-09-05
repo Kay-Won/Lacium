fa8e8af82bea98914b635f2e07dcd0a6be38abce
Kay Won <kandr3w8i6@gmail.com>
2025-09-06 00:21:10 +1000

    Setting up database

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,8 @@ function navbar() {
   return (
   <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-
   [backdrop-filter]:bg-background/60 z-50">
-    <div className="max-w-7xl mx-auto px-4">
+    {/*<div className="max-w-7xl mx-auto px-4">*/}
+    <div className="w-full px-20">
         <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
                 <Link href="/" className="text-xl text-primary --font-syncopate tracking-wider">
